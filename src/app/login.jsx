e3df3dbe89d1ac61e44b08f3cd639b56154a0a98
Miguel Enrique Alvarez Merlos <malvarezm26@miumg.edu.gt>
2023-09-26 15:43:26 -0600

    primer commit

---
+++ src/app/login.jsx
@@ -0,0 +1,12 @@
+
+const LoginPage =()=>{
+    return(
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/app/nota/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import NoteForm from "@/components/NoteForm";
+import NoteCard from "@/components/NoteCard";
+import { useNotes } from "@/context/NoteContext";
+import { useEffect } from "react";
+
+function HomePage() {
+  const { notes, loadNotes } = useNotes();
+
+  useEffect(() => {
+    loadNotes();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div>
+          <NoteForm />
+          {notes.map((note) => (
+            <NoteCard note={note} key={note.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
